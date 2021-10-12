aaa337e112f2874066b90af741ba66b990e9ef9f
davidwright <david@notunreasonable.com>
2021-10-12 14:47:03 -0400

    js

--- js/scripts.js
+++ js/scripts.js
@@ -22,8 +22,8 @@ fetch(RSS_URL2)
     console.log(data);
     console.log(data.querySelector("channel"))
     console.log(data.querySelector("channel").querySelector("title"))
-    console.log(data.querySelectorfor("channel.description"))
-    const items = data.querySelectorforAll("channel.item");
+    console.log(data.querySelector("channel.description"))
+    const items = data.querySelectorAll("channel.item");
 
     items.forEach(el => {
       console.log(el.querySelector("title"))
