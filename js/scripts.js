a969b0848b824a02357f6de45bdef036d86d4556
davidwright <david@notunreasonable.com>
2021-10-14 11:59:57 -0400

    debug

--- js/scripts.js
+++ js/scripts.js
@@ -181,10 +181,10 @@ function isValid() {
 window.addEventListener('DOMContentLoaded', event => {
 
   // load up the form fields
-  fields.name = document.getElementByID('name');
-  fields.email = document.getElementByID('email');
-  fields.phone = document.getElementByID('phone');
-  fields.message = document.getElementByID('message');
+  fields.name = document.getElementByID('name').value;
+  fields.email = document.getElementByID('email').value;
+  fields.phone = document.getElementByID('phone').value;
+  fields.message = document.getElementByID('message').value;
 
 
     // Navbar shrink function
