d882bababb14d2950992befd522c816900f667c9
davidwright <david@notunreasonable.com>
2021-10-16 07:23:46 -0400

    now with modal

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,17 @@
 
 var fields = {};
 var podcasts = {}
+var podcasts_obj = {}
+var baseItunesURL = 'https://itunes.apple.com/search?media=podcast'
 
 
+async function get_clip_with_podcast_index(podcast_index) {
+  RSS_URL =  podcasts_obj[podcast_index]['feedUrl']
+  await get_clip_with_RSS(RSS_URL)
 
-async function get_clip() {
+}
 
-
-
-  var podcast_name = document.getElementById('podcast_list').value;
-  console.log(podcast_name)
-  RSS_URL = podcasts[podcast_name]
-
-  console.log(RSS_URL)
+async function get_clip_with_RSS(RSS_URL) {
   document.getElementById("process_status").innerHTML = 'getting ' + RSS_URL
   // for a random element
   //const randomElement = array[Math.floor(Math.random() * array.length)];
@@ -70,6 +69,19 @@ async function get_clip() {
 
 
     })
+
+}
+
+
+async function get_clip() {
+
+  var podcast_name = document.getElementById('podcast_list').value;
+  console.log(podcast_name)
+  RSS_URL = podcasts[podcast_name]
+
+  console.log(RSS_URL)
+  await get_clip_with_RSS(RSS_URL)
+
   }
 
 function handleError() {
@@ -77,64 +89,218 @@ function handleError() {
 }
 
 
-function searchPodcasts() {
+function genlookalikes(podcast) {
+    var genre = podcasts[podcast]
+    getPodcasts(params)
+
+  }
+
+async function getPodcasts(params) {
+
+  return $.ajax({
+      url: baseItunesURL + params,
+      dataType: "jsonp",
+      success: function( response ) {
+          //console.log(response.results);
+          //var container = document.getElementById('search results');
+          //for (var member in podcasts) delete podcasts[member]
+          for (var member in podcasts_obj) delete podcasts_obj[member]
+
+          podcasts_obj = response.results;
+          console.log(podcasts_obj)
+          /*var datalist = document.createElement("select");
+          datalist.id = 'podcast_list';
+
+          for (let i = 0; i < podcasts_obj.length; i++) {
+
+                console.log(podcasts_obj[i]['collectionName']);
+                let name = podcasts_obj[i]['collectionName']
+                var newOptionElement = document.createElement("option");
+
+                newOptionElement.value = name
+                newOptionElement.text = name;
+                datalist.appendChild(newOptionElement);
+
+                }
+                var label = document.createElement("label");
+              label.innerHTML = "Choose your podcast for a clip! "
+              label.htmlFor = "podcast_list";
+
+              container.appendChild(datalist)
+              var submitter = document.createElement("button");
+              submitter.id = 'submit_podcast'
+              //submitter.onclick='get_clip()'
+              submitter.setAttribute( "onClick", "get_clip();" );
+              submitter.innerHTML="Get A Clip!"
+              submitter.setAttribute( "class", "btn-secondary" );
+              container.appendChild(submitter)
+              var label = document.createElement("br");
+              container.appendChild(label)
+
+            var suggester = document.createElement("button");
+            suggester.id = 'suggest_podcast'
+            //submitter.onclick='get_clip()'
+            suggester.setAttribute( "onClick", "suggest_podcast();" );
+            suggester.innerHTML="Suggest some other podcasts for me like this one!"
+            suggester.setAttribute( "class", "btn-secondary" );
+            container.appendChild(suggester)*/
+
+
+        }
+
+      }
+    );
+
+}
+
+
+async function savePodcasts() {
+
+  let form = new FormData();
+  form.append('podcasts_json',podcasts_obj);
+
+
+  fetch('https://harvesting.ninja/receive_podcast_list', {
+      method: 'POST',
+      body: form
+  })
+      .then(function(response) {
+          return response.text()
+          console.log('sent the file')
+
+      } )
+      .catch(function(error) {
+        console.log("there was an error")
+        console.log(error)
+        }
+      )
+
+}
+
+async function searchPodcasts() {
 
       //https://itunes.apple.com/search?
-      var baseURL = 'https://itunes.apple.com/search?media=podcast'
       var term = '&term=' + document.getElementById("selected_podcast").value
 
       var limit = '&limit=10'
       term = term.replace(' ', '+')
-      var params = term //+ callback
-      console.log(baseURL + params)
+      var params = term + limit
+      console.log(baseItunesURL + params)
       document.getElementById("process_status").innerHTML = '';
 
-      const myNode = document.getElementById("search results");
-      while (myNode.firstChild) {
-        myNode.removeChild(myNode.lastChild);
-      }
+      //const myNode = document.getElementById("search results");
+    //  while (myNode.firstChild) {
+      //  myNode.removeChild(myNode.lastChild);
+      //}
 
-      $.ajax({
-          url: baseURL + params,
-          dataType: "jsonp",
-          success: function( response ) {
-              //console.log(response.results);
-              var container = document.getElementById('search results');
-              for (var member in podcasts) delete podcasts[member]
+        await getPodcasts(params)
+    }
 
-              var datalist = document.createElement("select");
-              datalist.id = 'podcast_list';
-              for (let i = 0; i < response.results.length; i++) {
-                    console.log(response.results[i]['collectionName']);
-                    let name = response.results[i]['collectionName']
-                    podcasts[name] = response.results[i]['feedUrl']
-                    var newOptionElement = document.createElement("option");
 
-                    newOptionElement.value = name
-                    newOptionElement.text = name;
-                    datalist.appendChild(newOptionElement);
+  //$('#modal1').on('show.bs.modal', function (event) {
+async function modalshow() {
+        var term = '&term=' + document.getElementById("selected_podcast").value
 
-                    }
-                    var label = document.createElement("label");
-                  label.innerHTML = "Choose your podcast for a clip! "
-                  label.htmlFor = "podcast_list";
-                  console.log('podcast_array')
+        var limit = '&limit=10'
+        term = term.replace(' ', '+')
+        var params = term + limit
+        console.log(baseItunesURL + params)
+        document.getElementById("process_status").innerHTML = '';
 
-                  console.log(podcasts)
-                  container.appendChild(datalist)
-                  var submitter = document.createElement("button");
-                  submitter.id = 'submit_podcast'
-                  //submitter.onclick='get_clip()'
-                  submitter.setAttribute( "onClick", "get_clip();" );
-                  submitter.innerHTML="Get A Clip!"
-                  submitter.setAttribute( "class", "btn-secondary" );
-                  container.appendChild(submitter)
+        //const myNode = document.getElementById("search results");
+      //  while (myNode.firstChild) {
+        //  myNode.removeChild(myNode.lastChild);
+        //}
+        console.log('modal show function')
+        console.log(podcasts_obj);
+
+        getPodcasts(params).then(result=>{
+            savePodcasts()
+            console.log(podcasts_obj);
+
+            //var button = $(event.relatedTarget) // Button that triggered the modal
+            //var recipient = button.data('whatever') // Extract info from data-* attributes
+            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+
+            const myNode = document.getElementById("results_table_container");
+            while (myNode.firstChild) {
+              myNode.removeChild(myNode.lastChild);
+            }
+
+            let table = document.createElement("table");
+
+            table.setAttribute("class","table")
+            let tbody = document.createElement('tbody');
+            let td = document.createElement('td')
+
+            table.appendChild(tbody);
+            //let insert_text = ''
+            let thead = table.createTHead();
+
+            //let row = thead.insertRow();
+            let th = document.createElement("th");
+            let text = document.createTextNode('Name');
+            th.appendChild(text);
+            thead.appendChild(th)
+
+            th = document.createElement("th");
+            text = document.createTextNode('Get Clip!');
+            th.appendChild(text);
+            thead.appendChild(th)
+
+            th = document.createElement("th");
+            text = document.createTextNode('Get More!');
+            th.appendChild(text);
+            thead.appendChild(th)
+
+            for (let i = 0; i < podcasts_obj.length; i++) {
+
+              row = tbody.insertRow();
+              podcast_name = podcasts_obj[i]['collectionName']
+              td = document.createElement('td')
+              text = document.createTextNode(podcast_name);
+              td.appendChild(text);
+              row.appendChild(td);
+
+              // buttons
+
+              td = document.createElement('td')
+              td.setAttribute("class","btn-secondary")
+              newlink = document.createElement('button');
+              onclickfn = 'get_clip_with_podcast_index('+i+')'
+              newlink.setAttribute('onclick', onclickfn);
+              text = document.createTextNode('Try a clip!');
+              newlink.appendChild(text);
+              newlink.setAttribute('data-bs-dismiss', 'modal');
+              td.appendChild(newlink);
+              row.appendChild(td);
+
+              td = document.createElement('td')
+              td.setAttribute("class","btn-secondary")
+              newlink = document.createElement('button');
+              newlink.setAttribute('href', '');
+              newlink.setAttribute('data-bs-dismiss', 'modal');
+
+              text = document.createTextNode('More Podcasts Like This!');
+              newlink.appendChild(text);
+              td.appendChild(newlink);
+              row.appendChild(td);
+
 
             }
 
-          }
-        );
+            document.getElementById('results_table_container').appendChild(table)
+            document.getElementById('above_text').innerHTML = '<br>'
+
+          //})
+          $('#modal1').modal('show');
+      })
       }
+
+
+
+
 
 async function request_clip(download_url, episode_name) {
   let update_text = "<p>selected episode</p><p>Now I'm processing a clip for you. Hang in there!</p>"
