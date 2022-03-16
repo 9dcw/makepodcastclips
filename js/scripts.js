9c68ff1d90f6c53d02e54550a64f2826bbe856fe
9dcw <davecwright@gmail.com>
2022-03-16 09:02:31 -0500

    now can specify episode

--- js/scripts.js
+++ js/scripts.js
@@ -13,26 +13,93 @@ var podcasts_obj = {}
 var baseItunesURL = 'https://itunes.apple.com/search?media=podcast'
 
 
-async function get_clip_with_podcast_index(podcast_index) {
+async function get_clip_with_podcast_index(podcast_index,
+  episode_index=-1, start_time=-1, duration=-1) {
+  console.log('podcast '+String(podcast_index)+' episode '+String(episode_index)+' start:'+String(start_time)
+  +' duration'+String(duration))
 
-  RSS_URL = podcasts_obj[podcast_index]['feedUrl']
-  await get_clip_with_RSS(RSS_URL)
+  episodes = await getPodcastEpisodes(podcast_index)
 
+  if (episode_index === -1) {
+    // get a random episode index
+    episode_index = Math.floor(Math.random() * episodes.length);
+
+  }
+
+  // I need to pass episode
+  let selectedURL = episodes[episode_index]['url']
+  //let episode_name = episodes[episode_index]['title']
+
+  let episode_name = selectedURL.split('/')[selectedURL.split('/').length-1]
+  episode_name = episode_name.split('?')[0]
+  let episode_title = episodes[episode_index]['title']
+  let update_text = "<p>selected episode: " + episode_title + "</p><p>Now I'm processing a clip for you. Hang in there!</p>"
+  document.getElementById("process_status").innerHTML = update_text
+
+  console.log('url for extension:' + selectedURL)
+  console.log('name for saving:' + episode_name)
+
+
+  let clip_url = ''
+  let form = new FormData();
+  form.append('episode_url',selectedURL);
+  form.append('episode_name',episode_name);
+  if (start_time !== -1) {
+    form.append('start_time',start_time);
+
+  }
+  if (duration !== -1) {
+    form.append('duration',duration);
+
+  }
+
+
+  fetch('https://harvesting.ninja/process_rss', {
+      method: 'POST',
+      body: form
+  })
+      .then(function(response) {
+          return response.text()
+          document.getElementById("process_status").innerHTML = 'received clip'
+
+      } )
+      .catch(function(error) {
+
+        document.getElementById("process_status").innerHTML = 'error! ' + error
+        }
+      )
+      .then(function (text) {
+
+        console.log(text)
+        clip_url= text.replace(/\"/g, "")
+        console.log(clip_url)
+        document.getElementById("process_status")
+        .innerHTML = '<a href="'+clip_url+'"" target="_blank">Click here for the clip!</a>'
+
+
+      }).then(function () {
+        window.open(clip_url, '_blank')
+      })
 }
 
-async function get_clip_with_RSS(RSS_URL) {
+function handleError() {
+  return
+}
+
+
+async function getPodcastEpisodes(podcast_index, detail=false) {
+  RSS_URL = podcasts_obj[podcast_index]['feedUrl']
   document.getElementById("process_status").innerHTML = 'getting ' + RSS_URL
   // for a random element
   //const randomElement = array[Math.floor(Math.random() * array.length)];
   // then I send that link to the server to give me a clip
-  var episodes = [];
   console.log(RSS_URL)
 
   wrapper_url = 'https://harvesting.ninja/cors_workaround?destination='
   final_url = wrapper_url + RSS_URL
   console.log(final_url)
 
-  fetch(final_url)
+  var episodes = await fetch(final_url)
 
   .then(response => response.text())
   .catch(function(error) {
@@ -44,67 +111,40 @@ async function get_clip_with_RSS(RSS_URL) {
   .then(data => {
     console.log(data);
     document.getElementById("process_status").innerHTML = 'got data'
+    rssns = 'http://www.w3.org/2005/Atom'
 
-    //try {
-      const channel = data.querySelector("channel")
-      console.log('channel')
-      console.log(channel)
-      const items = channel.querySelectorAll("item");
-      console.log('items')
-    //}
+    const rss = data.querySelector("rss")
+    // I'm trying to get at the namespace in the rss target
+    // but that itself means I need the namespace of xmlns
+    // which isn't working!
+    console.log(rss.hasAttributeNS(rssns, 'itunes' ))
 
 
-    //catch(err) {
-    //  console.log('error!')
-    //  document.getElementById("process_status").innerHTML = JSON.stringify(data)
-
-    //}
-    console.log(items)
+    const channel = data.querySelector("channel")
+    console.log('channel')
+    //console.log(channel)
+    const items = channel.querySelectorAll("item");
+    console.log('items')
+    //console.log(items)
+    var episodes_inner = []
+    var ns = 'http://www.itunes.com/dtds/podcast-1.0.dtd';
 
     items.forEach(el => {
       //console.log(el.querySelector("title"))
-      try {
+      console.log(el)
+      console.log(el.hasAttributeNS(ns, 'duration' ))
+          episodes_inner.push({url: el.querySelector("enclosure").getAttribute('url'),
+                               title: el.querySelector("title").firstChild.nodeValue,
+                               duration: el.getAttributeNS(ns, 'duration' )
+                             })
+        })
 
-        episodes.push(el.querySelector("enclosure").getAttribute('url'))
 
-      //console.log(el.querySelector("podcast: transcript").url)
-
-        }
-        catch(err) {
-          console.log('no download for an episode')
-          document.getElementById("process_status").innerHTML = 'no download for this episode!'
-        }
-
-        }
-      )
-    }).then(function () {
-
-      var rndnum = Math.random()
-      console.log(episodes)
-      var rnd = Math.floor(Math.random() * episodes.length);
-      let sel = episodes[rnd]
-      clip_url = request_clip(sel, podcast_name)
-      // then I need to pull up that clip!
-    })
-
+    return episodes_inner
+  })
+  console.log(episodes)
+return episodes
 }
-
-
-async function get_clip() {
-
-  var podcast_name = document.getElementById('podcast_list').value;
-  console.log(podcast_name)
-  RSS_URL = podcasts[podcast_name]
-
-  console.log(RSS_URL)
-  await get_clip_with_RSS(RSS_URL)
-
-  }
-
-function handleError() {
-  return
-}
-
 
 function genlookalikes(podcast_index) {
   document.getElementById("process_status").innerHTML = 'getting your lookalikes, be right back!';
@@ -240,31 +280,14 @@ function toggleText() {
 
 }
 
-async function episodesmodal() {
-  var term = '&term=' + document.getElementById("selected_podcast").value
+async function episodesModal(podcast_index) {
 
-  var limit = '&limit=10'
-  term = term.replace(' ', '+')
-  var params = term + limit
-  console.log(baseItunesURL + params)
-
-  //const myNode = document.getElementById("search results");
-//  while (myNode.firstChild) {
-  //  myNode.removeChild(myNode.lastChild);
-  //}
   console.log('modal show function')
-  console.log(podcasts_obj);
+  //console.log(podcasts_obj);
 
-  getPodcasts(params).then(result=>{
+  getPodcastEpisodes(podcast_index).then(episodesObj=>{
+
       document.getElementById("process_status").innerHTML = '';
-
-      savePodcasts()
-      console.log(podcasts_obj);
-
-      //var button = $(event.relatedTarget) // Button that triggered the modal
-      //var recipient = button.data('whatever') // Extract info from data-* attributes
-      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
 
       const myNode = document.getElementById("results_table_container");
       while (myNode.firstChild) {
@@ -288,49 +311,47 @@ async function episodesmodal() {
       thead.appendChild(th)
 
       th = document.createElement("th");
-      text = document.createTextNode('Get Clip!');
+      text = document.createTextNode('Duration');
       th.appendChild(text);
       thead.appendChild(th)
 
       th = document.createElement("th");
-      text = document.createTextNode('Subscribe!');
+      text = document.createTextNode('Try Clip');
+      th.appendChild(text);
+      thead.appendChild(th)
+
+      th = document.createElement("th");
+      text = document.createTextNode('Link');
       th.appendChild(text);
       thead.appendChild(th)
 
 
-      for (let i = 0; i < podcasts_obj.length; i++) {
+      for (let k = 0; k < episodesObj.length; k++) {
 
         row = tbody.insertRow();
         row.setAttribute('vertical-align', 'middle')
-        podcast_name = podcasts_obj[i]['collectionName']
+        episode_name = episodesObj[k]['title']
+
         td = document.createElement('td')
         td.setAttribute('align', 'left')
 
-        text = document.createTextNode(podcast_name);
+        text = document.createTextNode(episode_name);
         td.appendChild(text);
 
         br = document.createElement('br')
         td.appendChild(br);
-
-        //"http://rss.acast.com/coffeebreakspanish"
-        moreText = document.createElement('a')
-        moreText.setAttribute('href', podcasts_obj[i]['collectionViewUrl']);
-        moreText.setAttribute('target', '_blank');
-
-        moreText.innerHTML = 'by: ' +podcasts_obj[i]['artistName']
-        td.appendChild(moreText);
-        //moreText.setAttribute('id', 'cast_text_id_'+i+'_toggle_text');
-        //moreText.innerHTML =podcasts_obj[i]['description']
-        //moreText.setAttribute('display', 'none')
-        //td.appendChild(moreText);
-
         row.appendChild(td);
-        // buttons
+
+        td = document.createElement('td')
+        td.setAttribute('align', 'left')
+        text = document.createTextNode(episodesObj[k]['duration']);
+        td.appendChild(text);
+        row.appendChild(td);
 
         td = document.createElement('td')
         td.setAttribute("class","btn-secondary")
         newlink = document.createElement('button');
-        onclickfn = 'get_clip_with_podcast_index('+i+')'
+        onclickfn = 'get_clip_with_podcast_index('+podcast_index+','+k+')'
         newlink.setAttribute('onclick', onclickfn);
         text = document.createTextNode('Try a clip!');
         newlink.appendChild(text);
@@ -341,10 +362,10 @@ async function episodesmodal() {
         td = document.createElement('td')
         td.setAttribute("class","btn-secondary")
         newlink = document.createElement('button');
-        newlink.setAttribute('href', podcasts_obj[i]['collectionViewUrl']);
+        newlink.setAttribute('href', episodesObj[k]['url']);
         newlink.setAttribute('target', '_blank');
         newlink.setAttribute('data-bs-dismiss', 'modal');
-        text = document.createTextNode('Subscribe!');
+        text = document.createTextNode('Play it!');
         newlink.appendChild(text);
         td.appendChild(newlink);
         row.appendChild(td);
@@ -354,8 +375,7 @@ async function episodesmodal() {
 
       document.getElementById('results_table_container').appendChild(table)
       document.getElementById('above_text').innerHTML = '<br>'
-
-    //})
+    console.log('showing modal?')
     $('#modal1').modal('show');
 
   })
@@ -496,7 +516,7 @@ async function showsModal() {
               td = document.createElement('td')
               td.setAttribute("class","btn-secondary")
               newlink = document.createElement('button');
-              onclickfn = 'getepisodes('+i+')'
+              onclickfn = 'episodesModal('+i+')'
               newlink.setAttribute('onclick', onclickfn);
               newlink.setAttribute('data-bs-dismiss', 'modal');
 
@@ -520,49 +540,6 @@ async function showsModal() {
 
 
 
-async function request_clip(download_url, episode_name) {
-  let update_text = "<p>selected episode: " + episode_name + "</p><p>Now I'm processing a clip for you. Hang in there!</p>"
-  document.getElementById("process_status").innerHTML = update_text
-
-  console.log('url for extension:' + download_url)
-  console.log('name for saving:' + episode_name)
-
-  let clip_url = ''
-  let form = new FormData();
-  form.append('episode_url',download_url);
-  form.append('episode_name',episode_name);
-
-
-  fetch('https://harvesting.ninja/process_rss', {
-      method: 'POST',
-      body: form
-  })
-      .then(function(response) {
-          return response.text()
-          document.getElementById("process_status").innerHTML = 'received clip'
-
-      } )
-      .catch(function(error) {
-
-        document.getElementById("process_status").innerHTML = 'error! ' + error
-        }
-      )
-      .then(function (text) {
-
-        console.log(text)
-        clip_url= text.replace(/\"/g, "")
-        console.log(clip_url)
-        document.getElementById("process_status")
-        .innerHTML = '<a href="'+clip_url+'"" target="_blank">Click here for the clip!</a>'
-
-
-      }).then(function () {
-        window.open(clip_url, '_blank')
-      })
-
-
-  return clip_url
-}
 
 
 function isNotEmpty(value) {
